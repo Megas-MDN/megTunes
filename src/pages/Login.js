ecfbecb73b3daad97ef1b34dae027d7d3a4b1550
Megas-MDN <mdn.melqui@gmail.com>
2022-11-28 13:46:59 -0300

    feat: styles

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -18,6 +18,10 @@ export default class Login extends Component {
       this.setState({
         isDisabled: false,
       });
+    } else {
+      this.setState({
+        isDisabled: true,
+      });
     }
   }
 
